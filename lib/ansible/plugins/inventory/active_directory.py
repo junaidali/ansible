ebdc26a9e2e236bad2291be0ee773cae3788061b
Syed Junaid Ali <syedjunaid.ali@slalom.com>
2019-12-03 15:09:17 -0600

    logging based on group type

--- lib/ansible/plugins/inventory/active_directory.py
+++ lib/ansible/plugins/inventory/active_directory.py
@@ -509,7 +509,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                     group_added_name = self.inventory.add_group(group_name)
                     self.inventory.add_child(group=group_added_name, child=hostname)
                     display.vvvv(
-                        "%s added to inventory group %s"
+                        "%s added to security group based inventory group %s"
                         % (hostname, group_name)
                     )
 
